f1ab2e87776f0c15e82000db92f3750e703e7a49
Asad Sunny <mdasadsunny@gmail.com>
2025-07-24 20:40:31 +0600

    feat: realloc all cases covered

--- example.c
+++ example.c
@@ -1,5 +1,6 @@
 #include "./src/dragonheap.h"
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   printf("%lu\n", META_SIZE);
@@ -14,9 +15,9 @@ int main() {
   dragon_release(mem);
   print_free_list();
 
-  // dragon heap first check for free chunks equal or less in se
+  // dragon heap first check for free chunks equal or less in size
   // should find a chunk but not big enough
-  // dragon heap should add this new chunk
+  // dragon heap should add this new chunk by expanding the heap
   printf("third action\n");
   void *mem2 = dragon_hoard(15);
 
@@ -28,7 +29,6 @@ int main() {
 
   // dragon heap first check for free chunks equal or less in size
   //  should find two chunks big enough to coalesce (15 + 13) = 28, more than enough
-  // should print this fact but still allocate additional 20 chunk
   printf("fifth action\n");
   void *mem3 = dragon_hoard(20);
   print_free_list();
@@ -37,19 +37,44 @@ int main() {
   dragon_release(mem3);
   print_free_list();
 
+  // testing realloc function so first we allcaote mem4 of 33 bytes
+  printf("seventh action\n");
   void *mem4 = dragon_hoard(33);
   print_free_list();
 
+  // then we want to resize the memory region from 33 to 60 bytes, anything below or equal to 33 should return an error
+  printf("eighth action\n");
   dragon_reforge(mem4, 60);
   print_dragon_heap();
+
+  // in the next two actions, we allocate and free the next adjacent chunk of memory to mem4 to test we can see upon further reallocation of mem4, does the realloc functions coalesces the next adjacent chunk, the chunk has to have enough size, if not enough size it sees if that insufficient chunk is at the program break, if so, it expands the heap
+  printf("ninth action\n");
   dragon_release(dragon_hoard(10));
   print_free_list();
   print_dragon_heap();
+
+  printf("tenth action\n");
   dragon_reforge(mem4, 100);
   print_free_list();
   print_dragon_heap();
+  // the next set of lines tests to see if the "wanting to realloc chunk" isn't at the program break so thus can't expand the heap, and its next adjacent chunks aren't free, then it finds a new chunk from the free list or by expanding the heap by using the dragon_hoard function inside its implemention, if such a case it's encountered the new chunk copies the data from the old chunk, and the old chunk is freed and its data is emptied, and the pointer to the new chunk's hoard (the actual region to used by the user) is returned
+  printf("eleventh action\n");
+  void *mem5 = dragon_hoard(15);
+  void *mem6 = dragon_hoard(10);
+  void *mem7 = dragon_hoard(5);
+  dragon_release(mem4);
+  printf("mem6: %p\n", mem6);
+  // checking to see if you can use the hoard or not
+  strcpy(mem6, "hello");
+  printf("mem6: %s\n", (char *)mem6);
+  mem6 = dragon_reforge(mem6, 100);
+  printf("mem6: %p\n", mem6);
+  // checking to see if the old data is transferred to this new region or not and can i add new data to it
+  strcpy(mem6 + 5, ", son!");
+  printf("mem6: %s\n", (char *)mem6);
 
-  // print_dragon_heap(dragon_heap);
+  print_dragon_heap();
+  print_free_list();
 
   return 0;
 }
--- src/dragonheap.c
+++ src/dragonheap.c
@@ -6,59 +6,6 @@
 extern memory_chunk_t *dragon_heap;
 extern free_list_t *dragon_free_list;
 
-// #define META_SIZE sizeof(memory_chunk_t)
-
-// free_list_t *dragon_free_list = {0};
-
-// void print_dragon_heap(memory_chunk_t *head) {
-//   printf("dragon_heap: ");
-//   memory_chunk_t *tmp = head;
-//   while (tmp != NULL) {
-//     if (tmp == head) {
-
-//       printf("(%lu, %lu, %lu, %d)", tmp, tmp->previous_chunk, tmp->size, tmp->is_free);
-//     } else {
-
-//       printf(" -> (%lu, %lu, %lu, %d)", tmp, tmp->previous_chunk, tmp->size, tmp->is_free);
-//     }
-//     tmp = tmp->previous_chunk;
-//   }
-//   printf("\n");
-// }
-
-// void print_free_list() {
-//   printf("free_list: ");
-//   free_list_t *iterator = dragon_free_list;
-//   while (iterator != NULL) {
-
-//     printf("(%lu, %lu) -> ", iterator, iterator->next_free_chunk);
-//     iterator = iterator->next_free_chunk;
-//   }
-//   printf("\n");
-// }
-
-// void rm_from_freelist(free_list_t *previous_node, free_list_t *node_to_delete) {
-//   if (previous_node != NULL) {
-//     previous_node->next_free_chunk = node_to_delete->next_free_chunk;
-
-//   } else {
-//     if (node_to_delete == dragon_free_list) {
-//       dragon_free_list = node_to_delete->next_free_chunk;
-//     } else {
-//       free_list_t *iterator = dragon_free_list;
-//       free_list_t *previous_free_chunk = NULL;
-//       while (iterator != NULL) {
-//         if (node_to_delete == iterator && previous_free_chunk) {
-//           previous_free_chunk->next_free_chunk = node_to_delete->next_free_chunk;
-//         }
-//         // printf("(%p, %p) -> ", iterator, iterator->next_free_chunk);
-//         previous_free_chunk = iterator;
-//         iterator = iterator->next_free_chunk;
-//       }
-//     }
-//   }
-// }
-
 void *dragon_hoard(size_t requested_size) {
 
   size_t received_size = requested_size;
@@ -74,7 +21,7 @@ void *dragon_hoard(size_t requested_size) {
 
     memory_chunk_t *free_chunk_meta = (void *)current_free_chunk - META_SIZE;
 
-    printf("(%lu, %lu) - ", free_chunk_meta, free_chunk_meta->previous_chunk);
+    printf("(%p, %p) - ", free_chunk_meta, free_chunk_meta->previous_chunk);
     printf("(is_free: %d, size: %lu) -> ", free_chunk_meta->is_free, free_chunk_meta->size);
 
     if (free_chunk_meta->size >= requested_size && free_chunk_meta->is_free) {
@@ -115,7 +62,7 @@ void *dragon_hoard(size_t requested_size) {
     previous_free_chunk = current_free_chunk;
     current_free_chunk = current_free_chunk->next_free_chunk;
   }
-  printf("\n\n");
+  printf("\n");
   if (chunk == NULL) {
     chunk = sbrk(requested_size + META_SIZE);
     dragon_heap = chunk;
@@ -128,26 +75,18 @@ void *dragon_hoard(size_t requested_size) {
     printf("memory allocation failed\n");
     return (void *)-1;
   }
-  // void *new_program_break = sbrk(0);
-  // printf("hoard: %p\n", chunk);
-  // printf("new program break: %p\n", new_program_break);
-  // size_t size_returned = new_program_break - chunk;
-  // printf("size_returned: %lu\n", size_returned);
-  // printf("size_usable: %lu\n", new_program_break - (chunk + META_SIZE));
-
-  // printf("current_head: %p\n", current_head);
   chunk->size = received_size;
   chunk->is_free = false;
 
   void *hoard = (void *)chunk + META_SIZE;
+  memset(hoard, 0, chunk->size);
 
   return hoard;
 }
 
-void *dragon_claim(void *hoard); // calloc
 void *dragon_reforge(void *hoard, size_t revised_size) {
   memory_chunk_t *current_chunk = (void *)hoard - META_SIZE;
-  if (revised_size < current_chunk->size) {
+  if (revised_size <= current_chunk->size) {
     return (void *)-1;
   }
 
@@ -176,7 +115,21 @@ void *dragon_reforge(void *hoard, size_t revised_size) {
       return hoard;
     }
   }
-  return NULL;
+
+  printf("getting a new hoard for you\n");
+  char *new_hoard = dragon_hoard(revised_size);
+
+  if (new_hoard == (void *)-1)
+    return new_hoard;
+
+  memory_chunk_t *new_chunk = (void *)new_hoard - META_SIZE;
+  if (new_chunk->size <= current_chunk->size)
+    return (void *)-1;
+
+  memcpy(new_hoard, hoard, current_chunk->size);
+  dragon_release(hoard);
+
+  return new_hoard;
 }
 void dragon_release(void *hoard) {
 
